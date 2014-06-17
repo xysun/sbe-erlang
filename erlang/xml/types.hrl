9f51ca3d9ee53f5a9fd7d5e3a95ddd25f0d0bf57
Xiayun Sun <xiayunsun@gmail.com>
2014-06-17 09:12:21 +0800

    cleanup

--- erlang/xml/types.hrl
+++
@@ -1,6 +0,0 @@
--record(primitiveType, {name,
-                         size,
-                         minValue,
-                         maxValue,
-                         nullValue
-                        }).
--- simple-binary-encoding/main/java/uk/co/real_logic/sbe/ir/generated/MetaAttribute.java
+++
@@ -1,9 +0,0 @@
-/* Generated SBE (Simple Binary Encoding) message codec */
-package uk.co.real_logic.sbe.ir.generated;
-
-public enum MetaAttribute
-{
-    EPOCH,
-    TIME_UNIT,
-    SEMANTIC_TYPE
-}
